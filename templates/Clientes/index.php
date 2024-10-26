33efa3223ecff70ef21913eb1f035852968f450b
Julyano <julykramer@hotmail.com>
2024-10-26 00:43:05 -0300

    Aplicando tema no layout e listagem de clientes

--- templates/Clientes/index.php
+++ templates/Clientes/index.php
@@ -1,10 +1,63 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var iterable<\App\Model\Entity\Cliente> $clientes
- */
-?>
-<div class="clientes index content">
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Listagem de Clientes</h3>
+                <?= $this->Html->link(__('New Cliente'), ['action' => 'add'], ['class' => 'button float-right']) ?>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th><?= $this->Paginator->sort('id') ?></th>
+                            <th><?= $this->Paginator->sort('nome') ?></th>
+                            <th><?= $this->Paginator->sort('email') ?></th>
+                            <th><?= $this->Paginator->sort('telefone') ?></th>
+                            <th><?= $this->Paginator->sort('excluido') ?></th>
+                            <th><?= $this->Paginator->sort('created') ?></th>
+                            <th><?= $this->Paginator->sort('modified') ?></th>
+                            <th class="actions"><?= __('Actions') ?></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($clientes as $cliente): ?>
+                            <tr>
+                                <td><?= $this->Number->format($cliente->id) ?></td>
+                                <td><?= h($cliente->nome) ?></td>
+                                <td><?= h($cliente->email) ?></td>
+                                <td><?= h($cliente->telefone) ?></td>
+                                <td><?= h($cliente->excluido) ?></td>
+                                <td><?= h($cliente->created) ?></td>
+                                <td><?= h($cliente->modified) ?></td>
+                                <td class="actions">
+                                    <?= $this->Html->link(__('View'), ['action' => 'view', $cliente->id]) ?>
+                                    <?= $this->Html->link(__('Edit'), ['action' => 'edit', $cliente->id]) ?>
+                                    <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $cliente->id], ['confirm' => __('Are you sure you want to delete # {0}?', $cliente->id)]) ?>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.card-body -->
+            <div class="card-footer clearfix">
+                <ul class="pagination pagination-sm m-0 float-right">
+                    <li class="page-item"><a class="page-link" href="#">«</a></li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">»</a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- /.card -->
+    </div>
+    <!-- /.col -->
+</div>
+
+
+<!-- <div class="clientes index content">
     <?= $this->Html->link(__('New Cliente'), ['action' => 'add'], ['class' => 'button float-right']) ?>
     <h3><?= __('Clientes') ?></h3>
     <div class="table-responsive">
@@ -23,20 +76,20 @@
             </thead>
             <tbody>
                 <?php foreach ($clientes as $cliente): ?>
-                <tr>
-                    <td><?= $this->Number->format($cliente->id) ?></td>
-                    <td><?= h($cliente->nome) ?></td>
-                    <td><?= h($cliente->email) ?></td>
-                    <td><?= h($cliente->telefone) ?></td>
-                    <td><?= h($cliente->excluido) ?></td>
-                    <td><?= h($cliente->created) ?></td>
-                    <td><?= h($cliente->modified) ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $cliente->id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $cliente->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $cliente->id], ['confirm' => __('Are you sure you want to delete # {0}?', $cliente->id)]) ?>
-                    </td>
-                </tr>
+                    <tr>
+                        <td><?= $this->Number->format($cliente->id) ?></td>
+                        <td><?= h($cliente->nome) ?></td>
+                        <td><?= h($cliente->email) ?></td>
+                        <td><?= h($cliente->telefone) ?></td>
+                        <td><?= h($cliente->excluido) ?></td>
+                        <td><?= h($cliente->created) ?></td>
+                        <td><?= h($cliente->modified) ?></td>
+                        <td class="actions">
+                            <?= $this->Html->link(__('View'), ['action' => 'view', $cliente->id]) ?>
+                            <?= $this->Html->link(__('Edit'), ['action' => 'edit', $cliente->id]) ?>
+                            <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $cliente->id], ['confirm' => __('Are you sure you want to delete # {0}?', $cliente->id)]) ?>
+                        </td>
+                    </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
@@ -49,6 +102,7 @@
             <?= $this->Paginator->next(__('next') . ' >') ?>
             <?= $this->Paginator->last(__('last') . ' >>') ?>
         </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?>
+        </p>
     </div>
-</div>
+</div> -->
